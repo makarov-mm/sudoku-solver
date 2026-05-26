29097a00fab27ff3f4b4c68adc6e1f2c3bfb8ee7
makarov-mm <m.m.makarov@gmail.com>
2026-05-27 00:58:50 +0200

    Add solver.

--- SudokuModel.cs
+++ SudokuModel.cs
@@ -1,29 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace SudokuSolver;
+﻿namespace SudokuSolver;
 
 public sealed class SudokuModel
 {
+    public const int BlockSize = 3;
     public const int BoardSize = 9;
     public int[,] Board = new int[BoardSize, BoardSize];
 
     public static SudokuModel GenerateExample()
     {
-        var model = new SudokuModel();
-
-        model.Board = new int[BoardSize, BoardSize]
+        var model = new SudokuModel
         {
-            { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
-            { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
-            { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
-            { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
-            { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
-            { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
-            { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
-            { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
-            { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+            Board = new[,]
+            {
+                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+                { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+                { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+                { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+                { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+                { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+                { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+                { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+            }
         };
 
         return model;
@@ -31,6 +29,174 @@ public sealed class SudokuModel
 
     public void Solve()
     {
+        var field = new HashSet<int>[BoardSize, BoardSize];
 
+        for (int i = 0; i < BoardSize; ++i)
+        {
+            for (int j = 0; j < BoardSize; ++j)
+            {
+                if (Board[i, j] == 0)
+                {
+                    field[i, j] = GenValues(BoardSize).ToHashSet();
+                }
+                else
+                {
+                    field[i, j] = [Board[i, j]];
+                }
+            }
+        }
+
+        bool changed = true;
+
+        while (changed)
+        {
+            changed = false;
+
+            for (int i = 0; i < BoardSize; ++i)
+            {
+                for (int j = 0; j < BoardSize; ++j)
+                {
+                    HashSet<int> set = field[i, j];
+                    var updated = new HashSet<int>();
+
+                    foreach (int value in set)
+                    {
+                        if (IsValid(i, j, value))
+                        {
+                            updated.Add(value);
+                        }
+                        else
+                        {
+                            changed = true;
+                        }
+                    }
+
+                    if (changed)
+                    {
+                        field[i, j] = updated;
+                    }
+                }
+            }
+
+            for (int i = 0; i < BoardSize; ++i)
+            {
+                for (int j = 0; j < BoardSize; ++j)
+                {
+                    if (Board[i, j] == 0 && field[i, j].Count == 1)
+                    {
+                        Board[i, j] = field[i, j].First();
+                        changed = true;
+                    }
+                }
+            }
+
+            if (!changed)
+            {
+                for (int i = 0; i < BoardSize && !changed; ++i)
+                {
+                    for (int j = 0; j < BoardSize && !changed; ++j)
+                    {
+                        if (Board[i, j] == 0)
+                        {
+                            HashSet<int> set = GetRandomValues(field[i, j]).ToHashSet();
+
+                            foreach (int value in set)
+                            {
+                                if (IsValid(i, j, value))
+                                {
+                                    Board[i, j] = value;
+                                    field[i, j].Remove(value);
+                                    changed = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
+
+    public bool IsSolved()
+    {
+        if (!IsValid())
+        {
+            return false;
+        }
+
+        for (int i = 0; i < BoardSize; ++i)
+        {
+            for (int j = 0; j < BoardSize; ++j)
+            {
+                if (Board[i, j] == 0) return false;
+                if (!IsValid(i, j, Board[i, j])) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public bool IsValid()
+    {
+        for (int i = 0; i < BoardSize; ++i)
+        {
+            for (int j = 0; j < BoardSize; ++j)
+            {
+                if (!IsValid(i, j, Board[i, j])) return false;
+            }
+        }
+
+        return false;
+    }
+
+    private bool IsValid(int x, int y, int value)
+    {
+        if (x is < 0 or >= BoardSize) return false;
+        if (y is < 0 or >= BoardSize) return false;
+
+        for (int i = 0; i < BoardSize; ++i)
+        {
+            if (i != x && Board[i, y] == value) return false;
+        }
+
+        for (int j = 0; j < BoardSize; ++j)
+        {
+            if (j != y && Board[x, j] == value) return false;
+        }
+
+        int blockX = (int)Math.Floor(x / (float)BlockSize);
+        int blockY = (int)Math.Floor(y / (float)BlockSize);
+
+        for (int i = blockX * BlockSize; i < ((blockX + 1) * BlockSize) && i < BoardSize; ++i)
+        {
+            for (int j = blockY * BlockSize; j < ((blockY + 1) * BlockSize) && j < BoardSize; ++j)
+            {
+                if (i != x && j != y && Board[i, j] == value) return false;
+            }
+        }
+
+        return true;
+    }
+
+    private IEnumerable<int> GenValues(int count)
+    {
+        for (int i = 1; i <= count; ++i)
+        {
+            yield return i;
+        }
+    }
+
+    private IEnumerable<int> GetRandomValues(HashSet<int> set)
+    {
+        set = new HashSet<int>(set.ToArray());
+
+        while (set.Count > 0)
+        {
+            int value = set.ToArray()[Rnd.Next(0, set.Count)];
+            set.Remove(value);
+            yield return value;
+        }
+    }
+
+    private static readonly Random Rnd = new();
 }
